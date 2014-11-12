f8f35bb110cb7d773eff948cf854a4d4c27149f6
vaseninm <vasenin@pixelio.ru>
2014-11-12 16:11:09 +0300

    catberry

--- app/app/collections/posts.js
+++
@@ -1,8 +0,0 @@
-var Post = require('../models/post')
-  , Base = require('./base');
-
-module.exports = Base.extend({
-    model: Post,
-    url: '/posts'
-});
-module.exports.id = 'Posts';
--- app/app/views/posts/list.js
+++
@@ -1,6 +0,0 @@
-var BaseView = require('../base');
-
-module.exports = BaseView.extend({
-  className: 'posts_list'
-});
-module.exports.id = 'posts/list';
