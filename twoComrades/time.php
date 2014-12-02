4bea77b4be4e6230da1865b789d85fb32ce5369f
Matvey Vasenin <vasenin@pixelio.ru>
2014-12-02 08:53:40 +0300

    timer fixing

--- twoComrades/time.php
+++ twoComrades/time.php
@@ -1,2 +1,3 @@
 <?php
+date_default_timezone_set('Etc/UCT');
 echo time();
--- twoComrades/timer.js
+++ twoComrades/timer.js
@@ -14,3 +14,19 @@ this.prototype.init = function() {
 
 };
 
+this.prototype.sync = function() {
+    var self = this;
+    $.get(this.timeUrl, {}, function(result) {
+        self.serverTime = result;
+    });
+};
+
+this.prototype.setTimeZone = function(timeZone) {
+    this.timeZone = timeZone;
+};
+
+this.prototype.getServerTime = function() {
+    var time = this.serverTime + this.timeZone;
+
+    return time;
+};
